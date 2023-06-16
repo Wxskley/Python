418c4875e1f47c780702ba03bd9de20e9292ce36
Wxskley <weskleydsilva@yahoo.com.br>
2023-06-16 16:13:27 -0300

    projeto a5 comentado

--- Projetos em Python para Iniciantes/password_generator.py
+++ Projetos em Python para Iniciantes/password_generator.py
@@ -3,22 +3,35 @@ import string
 
 
 def password_generator(len_pass=8):
-    ascii_options = string.ascii_letters
-    number_options = string.digits
-    punt_options = string.punctuation
+    # Define as opções de caracteres para a geração da senha
+    ascii_options = string.ascii_letters  # Letras maiúsculas e minúsculas
+    number_options = string.digits  # Números de 0 a 9
+    punt_options = string.punctuation  # Caracteres especiais
     options = ascii_options + number_options + punt_options
-    password_user = ""
-    for i in range(0, len_pass):
-        digit = random.choice(options)
-        password_user = password_user + digit
-    return password_user
+
+    password_user = ""  # Variável para armazenar a senha gerada
+
+    for i in range(0, len_pass):  # Loop para gerar cada dígito da senha
+        digit = random.choice(
+            options
+        )  # Escolhe um caractere aleatório das opções disponíveis
+        password_user = password_user + digit  # Adiciona o caractere à senha
+
+    return password_user  # Retorna a senha gerada
 
 
-choice_user = input("Quantos dígitos na senha?")
-if choice_user.isdigit():
-    choice_user = int(choice_user)
+choice_user = input(
+    "Quantos dígitos na senha?"
+)  # Solicita ao usuário o comprimento desejado da senha
+if choice_user.isdigit():  # Verifica se a entrada é um número inteiro
+    choice_user = int(choice_user)  # Converte a entrada para um número inteiro
 else:
-    print("Entrada inválida!")
-    quit()
-response = password_generator(len_pass=choice_user)
-print(f"Senha gerada:\n{response}")
+    print(
+        "Entrada inválida!"
+    )  # Exibe uma mensagem de erro se a entrada não for um número
+    quit()  # Encerra o programa
+
+response = password_generator(
+    len_pass=choice_user
+)  # Chama a função password_generator() passando o comprimento escolhido
+print(f"Senha gerada:\n{response}")  # Exibe a senha gerada na tela
